7f2e6bba7d9046ec796864fc5b0d82626b58cab5
Tùng Trần <51732119+tranthanhtung04101995@users.noreply.github.com>
2020-07-20 08:37:35 +0700

    commit

--- src/main/java/com/vnnet/test/console/Test.java
+++ src/main/java/com/vnnet/test/console/Test.java
@@ -1,36 +1,32 @@
 package com.vnnet.test.console;
 
 import org.aarboard.nextcloud.api.*;
-import org.apache.commons.httpclient.Credentials;
-import org.apache.commons.httpclient.HttpClient;
-import org.apache.commons.httpclient.HttpException;
-import org.apache.commons.httpclient.UsernamePasswordCredentials;
-import org.apache.commons.httpclient.auth.AuthScope;
-import org.apache.commons.httpclient.methods.GetMethod;
 
-import java.io.*;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
 public class Test {
     public static void main(String[] args) {
         final String baseUrl = "https://tung95.duckdns.org//remote.php/webdav";
         try {
             NextcloudConnector nextcloudConnector = new NextcloudConnector("tung95.duckdns.org", true, 443, "nc_admin", "nextcloudpassword");
-            InputStream initialStream = new FileInputStream(
-                    new File("/home/t94hp/Desktop/Coast.jpg"));
-//            nextcloudConnector.uploadFile( initialStream,"/Photos/Coast2.jpg");
+            // download file vve may
 //            nextcloudConnector.downloadFile("/Photos/Coast1.jpg","/home/t94hp/Desktop/");
-            nextcloudConnector.createFolder("nc_admin/Musics");
-//            InputStream inputStream  = nextcloudConnector.downloadFile("/Photos/Coast1.jpg");
-//            String fileName = "test.jpg";
-//            FileOutputStream fos = new FileOutputStream(fileName);
-//            int i = IOUtils.copy(inputStream, fos);
-//            System.out.println(i);
-        } catch (IOException e) {
+            // upload file len he thong
+//            InputStream initialStream = new FileInputStream(new File("/home/t94hp/Desktop/cogiao.jpg"));
+//            nextcloudConnector.uploadFile(initialStream,"/Photos/cogiao.jpg");
+            //Tao 1 user moi
+//            boolean kq = nextcloudConnector.createUser("qmstung","qmstung");
+//            System.out.println();
+            //Tao moi thu muc cho quan tri vien
+//            nextcloudConnector.createFolder("Musics");
+//            nextcloudConnector.deleteFolder("Musics");
+            boolean result = nextcloudConnector.createUser("TESTUSER", "aBcDeFg123456");
+
+        } catch (Exception e) {
             e.printStackTrace();
         }
-
     }
-
 }
